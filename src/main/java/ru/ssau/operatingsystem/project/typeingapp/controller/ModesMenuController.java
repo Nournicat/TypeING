77e90d4b09e37a93b95336e6cc94822e8c7ad98e
Nournic <ploxind@bk.ru>
2025-03-15 13:37:02 +0400

    .

--- src/main/java/ru/ssau/operatingsystem/project/typeingapp/controller/ModesMenuController.java
+++ src/main/java/ru/ssau/operatingsystem/project/typeingapp/controller/ModesMenuController.java
@@ -6,38 +6,13 @@ import javafx.scene.Cursor;
 import javafx.scene.Scene;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
-import ru.ssau.operatingsystem.project.typeingapp.MainApp;
-import ru.ssau.operatingsystem.project.typeingapp.RandomString;
-import ru.ssau.operatingsystem.project.typeingapp.RandomTextProvider;
+import ru.ssau.operatingsystem.project.typeingapp.*;
 import ru.ssau.operatingsystem.project.typeingapp.utility.Utility;
 
 import java.io.IOException;
 import java.security.SecureRandom;
 
 public class ModesMenuController {
-
-    @FXML
-    private AnchorPane pane1;
-
-    @FXML
-    void pane1Click(MouseEvent event) {
-        Scene scene;
-        FXMLLoader fxmlLoader = new FXMLLoader(MainApp.class.getResource("3.fxml"));
-        try {
-            scene = new Scene(fxmlLoader.load(), 600, 400);
-        } catch (IOException e) {
-            System.out.println("Ошибка");
-            throw new RuntimeException(e);
-        }
-
-//        String easy = "абвгд";
-//        RandomString stringGenerator = new RandomString(4, new SecureRandom(), easy);
-//        RandomTextProvider textProvider = new RandomTextProvider(10, stringGenerator);
-//        System.out.println(textProvider.generate());
-
-        Utility.changeScene(scene);
-    }
-
     @FXML
     void mouseChangeEventEnter(MouseEvent event) {
         Utility.changeCursor(Cursor.HAND);
@@ -73,7 +48,12 @@ public class ModesMenuController {
 
     @FXML
     void mouseClickEventEnglishAlphabet(MouseEvent event) {
+        RandomStringTextProvider textProvider = wordsGenerator(1, new String[]{
+                        "abcdefghijklmnopqrstuvwxyz"
+                }
+        );
 
+        Utility.startTyping(textProvider);
     }
 
     @FXML
@@ -95,7 +75,14 @@ public class ModesMenuController {
 
     @FXML
     void mouseClickEventProgrammingJava(MouseEvent event) {
+        String[] words = new String[]{
+            "int", "public static void", "String", "new ArrayList<>()",
+                    "Double", "Arrays.of(1, 2, 3).stream().min().get();",
+                    "System.out.println(\"Hello World\");", "Random random = new Random();"
+        };
+        RandomStringTextProvider textProvider = wordsGenerator(words.length, words);
 
+        Utility.startTyping(textProvider);
     }
 
     @FXML
@@ -105,7 +92,12 @@ public class ModesMenuController {
 
     @FXML
     void mouseClickEventRussianAlphabet(MouseEvent event) {
+        RandomStringTextProvider textProvider = wordsGenerator(1, new String[]{
+                "абвгдеёжзийклмнопрстуфхцчшщъыьэюя"
+        }
+        );
 
+        Utility.startTyping(textProvider);
     }
 
     @FXML
@@ -124,6 +116,10 @@ public class ModesMenuController {
         RandomString stringGenerator = new RandomString(6, new SecureRandom(), str);
 
         return new RandomTextProvider(10, stringGenerator);
+    }
+
+    RandomStringTextProvider wordsGenerator(int count, String[] words){
+        return new RandomStringTextProvider(count, words);
     }
 
 }
