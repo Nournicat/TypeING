c99bbee790e700059ab63225e14ff19bac5dff20
Gealor <gzazulya@gmail.com>
2025-05-05 13:35:52 +0400

    added accuracy

--- src/main/java/ru/ssau/operatingsystem/project/typeingapp/utility/stats/TypingStats.java
+++ src/main/java/ru/ssau/operatingsystem/project/typeingapp/utility/stats/TypingStats.java
@@ -1,11 +1,17 @@
 package ru.ssau.operatingsystem.project.typeingapp.utility.stats;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
 public class TypingStats implements IStatistic {
     private int characterCount;
     private int wordCount;
     private double wpm;
     private double spm;
     private int errorCount;
+    private double accuracy;
 
     public TypingStats(int characterCount, int wordCount, double wpm, double spm, int errorCount){
         this.characterCount = characterCount;
@@ -15,39 +21,6 @@ public class TypingStats implements IStatistic {
         this.errorCount = errorCount;
     }
 
-    public void setCharacterCount(int characterCount){
-        this.characterCount = characterCount;
-    }
 
-    public void setWordCount(int wordCount){
-        this.wordCount = wordCount;
-    }
-
-    public void setWpm(double wpm){
-        this.wpm = wpm;
-    }
-
-    public void setSpm(double spm){
-        this.spm = spm;
-    }
-
-    public void setErrorCount(int errorCount){
-        this.errorCount = errorCount;
-    }
-
-    public int getCharacterCount(){
-        return characterCount;
-    }
-
-    public int getWordCount(){
-        return wordCount;
-    }
-
-    public double getWpm(){
-        return wpm;
-    }
-    public double getSpm(){ return spm; }
-
-    public int getErrorCount(){ return errorCount; }
 
 }
